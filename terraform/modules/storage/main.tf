1ee488d74bf333599c526593b44655b2853446cf
Alamin Islam <alaminislam@Alamins-MBP.lan>
2025-08-23 14:44:20 +0100

    Add S3 versioning and KMS policies

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,34 +1,52 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_kms_key" "dynamodb" {
-  description         = "KMS key for DynamoDB encryption"
-  enable_key_rotation = true
-  tags                = var.tags
+ description         = "KMS key for DynamoDB encryption"
+ enable_key_rotation = true
+ 
+ policy = jsonencode({
+   Version = "2012-10-17"
+   Statement = [
+     {
+       Sid    = "Enable IAM User Permissions"
+       Effect = "Allow"
+       Principal = {
+         AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+       }
+       Action   = "kms:*"
+       Resource = "*"
+     }
+   ]
+ })
+ 
+ tags = var.tags
 }
 
 resource "aws_kms_alias" "dynamodb" {
-  name          = "alias/${var.name_prefix}-dynamodb"
-  target_key_id = aws_kms_key.dynamodb.key_id
+ name          = "alias/${var.name_prefix}-dynamodb"
+ target_key_id = aws_kms_key.dynamodb.key_id
 }
 
 resource "aws_dynamodb_table" "urls" {
-  name         = "${var.name_prefix}-urls"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "short_code"
+ name         = "${var.name_prefix}-urls"
+ billing_mode = "PAY_PER_REQUEST"
+ hash_key     = "short_code"
 
-  attribute {
-    name = "short_code"
-    type = "S"
-  }
+ attribute {
+   name = "short_code"
+   type = "S"
+ }
 
-  point_in_time_recovery {
-    enabled = true
-  }
+ point_in_time_recovery {
+   enabled = true
+ }
 
-  server_side_encryption {
-    enabled     = true
-    kms_key_arn = aws_kms_key.dynamodb.arn
-  }
+ server_side_encryption {
+   enabled     = true
+   kms_key_arn = aws_kms_key.dynamodb.arn
+ }
 
-  tags = merge(var.tags, {
-    Name = "${var.name_prefix}-urls"
-  })
+ tags = merge(var.tags, {
+   Name = "${var.name_prefix}-urls"
+ })
 }
