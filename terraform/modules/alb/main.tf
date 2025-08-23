a509034f94682284a7497cba3a09be887b6a1dd0
Alamin Islam <alaminislam@Alamins-MBP.lan>
2025-08-23 17:25:15 +0100

    Fix ALB S3 permissions and WAF logging ARN format

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -338,7 +338,13 @@ resource "aws_s3_bucket_policy" "alb_logs" {
         Principal = {
           AWS = data.aws_elb_service_account.main.arn
         }
-        Action   = "s3:PutObject"
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = data.aws_elb_service_account.main.arn
+        }
+        Action   = "s3:GetBucketAcl"        Action   = "s3:PutObject"
         Resource = "${aws_s3_bucket.alb_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
       }
     ]
